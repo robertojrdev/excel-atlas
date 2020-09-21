99f8db8e7cad860d2148b523034e57c36a8c4361
Roberto <junior.rmg.rj@gmail.com>
2020-09-22 00:55:33 +0100

    icon updated

--- maps.js
+++ maps.js
@@ -38,7 +38,7 @@ function loadMarkers() {
             {
                 item.marker = new google.maps.Marker({
                     position: pos,
-                    icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/info-i_maps.png"
+                    icon: "https://github.com/robertojrdev/excel-atlas/blob/master/turistic-icon.png"
                 });
             }
 
